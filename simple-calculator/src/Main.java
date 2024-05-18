f4c73ce6ce7282f819405a52f08b96e0e124b173
Muhammad Zulfahmi <85464331+zulluzi@users.noreply.github.com>
2024-05-18 16:01:03 +0700

    Update Main.java

--- simple-calculator/src/Main.java
+++ simple-calculator/src/Main.java
@@ -19,8 +19,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner scan = new Scanner(System.in);
         int a = 0;
         int b = 0;
